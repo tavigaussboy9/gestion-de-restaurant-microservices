4981d436ec6af00a2c4e3b9023e22585fb2ab0e6
tavigaussboy9 <prieuxolowo@hotmail.com>
2025-09-27 16:10:16 +0400

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue dans le service de gestion de restaurant!');
+});
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
